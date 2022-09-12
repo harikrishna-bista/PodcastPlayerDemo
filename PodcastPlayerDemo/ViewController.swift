1fed50e48269ad0eb45fd08e3f3cbbd51d5d67c3
Harikrishna bista <harikrishna.bista@ebpearls.com>
2022-09-12 21:42:42 +0545

    create demo for podcast player

--- PodcastPlayerDemo/ViewController.swift
+++ PodcastPlayerDemo/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import PodcastPlayer
+
 
 class ViewController: UIViewController {
+    
+    var items: [PlayerItem] = [
+        PlayerItem(itemURL: URL(string: "https://www.thepodcastexchange.ca/s/AlanCross-Porter-v2.mp3")!, thumbnailURL: URL(string: "http://vocaro.com/trevor/blog/wp-content/uploads/2009/10/Resize-with-interpolation.jpg"), albumName: nil, trackName: nil),
+        PlayerItem(itemURL: URL(string: "https://www.thepodcastexchange.ca/s/McDonalds_LNG_061019.wav")!, thumbnailURL: nil, albumName: nil, trackName: nil),
+        PlayerItem(itemURL: URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!, thumbnailURL: nil, albumName: nil, trackName: nil),
+        PlayerItem(itemURL: URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")!, thumbnailURL: nil, albumName: "Elephant", trackName: "Dreams"),
+        PlayerItem(itemURL: URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!, thumbnailURL: nil, albumName: nil, trackName: nil),
+        
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+   
+    @IBAction func showPodcastPlayer(_ sender: Any) {
+        let podcastPlayerVC = PlayerViewController(playerView: PodcastPlayerView())
+        podcastPlayerVC.dataSource = self
+        podcastPlayerVC.delegate = self
+        navigationController?.pushViewController(podcastPlayerVC, animated: true)
+    }
+    
 }
-
+//MARK: - PlayerViewControllerDataSource
+extension ViewController: PlayerViewControllerDataSource {
+    func numberOfPlayerItems(in controller: PlayerViewController) -> Int {
+        return items.count
+    }
+    
+    func playerViewController(_ controller: PlayerViewController, itemAtIndex index: Int) -> PlayerItem? {
+        return items[index]
+    }
+}
+// MARK: - PlayerViewControllerDelegate
+extension ViewController: PlayerViewControllerDelegate {
+    func playerViewController(_ controller: PlayerViewController, didSkipForwardOnItem item: PlayerItem, atIndex index: Int) {
+        
+    }
+    func playerViewController(_ controller: PlayerViewController, didSkipBackwardOnItem item: PlayerItem, atIndex index: Int) {
+        
+    }
+    func playerViewController(_ controller: PlayerViewController, didStartPlayingItem item: PlayerItem, atIndex index: Int) {
+        
+    }
+    func playerViewController(_ controller: PlayerViewController, didPauseItem item: PlayerItem, atIndex index: Int) {
+        
+    }
+    func playerViewController(_ controller: PlayerViewController, didSkipItemAtIndex index: Int, withReason reason: PlayerViewError) {
+        
+    }
+    
+}
