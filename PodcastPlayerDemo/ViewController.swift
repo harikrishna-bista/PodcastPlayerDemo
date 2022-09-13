98752124e4bedb56644d2d36c12188fcecbd2cfa
Harikrishna bista <harikrishna.bista@ebpearls.com>
2022-09-13 09:29:46 +0545

    added model to test

--- PodcastPlayerDemo/ViewController.swift
+++ PodcastPlayerDemo/ViewController.swift
@@ -12,11 +12,13 @@ import PodcastPlayer
 class ViewController: UIViewController {
     
     var items: [PlayerItem] = [
-        PlayerItem(itemURL: URL(string: "https://www.thepodcastexchange.ca/s/AlanCross-Porter-v2.mp3")!, thumbnailURL: URL(string: "http://vocaro.com/trevor/blog/wp-content/uploads/2009/10/Resize-with-interpolation.jpg"), albumName: nil, trackName: nil),
-        PlayerItem(itemURL: URL(string: "https://www.thepodcastexchange.ca/s/McDonalds_LNG_061019.wav")!, thumbnailURL: nil, albumName: nil, trackName: nil),
+        PlayerItem(itemURL: URL(string: "https://www.thepodcastexchange.ca/s/AlanCross-Porter-v2.mp3")!, thumbnailURL: .url(URL(string: "http://vocaro.com/trevor/blog/wp-content/uploads/2009/10/Resize-with-interpolation.jpg")!) ,
+                   albumName: nil, trackName: nil),
+        PlayerItem(itemURL: URL(string: "https://www.thepodcastexchange.ca/s/McDonalds_LNG_061019.wav")!, thumbnailURL: .image(UIImage(named: "thumbnail")!), albumName: nil, trackName: nil),
+        PlayerItem(itemURL: URL(string: "https://www.thepodcastexchange.ca/s/Allusionist-HSBC-PRE-2019-07-12.mp3")!, thumbnailURL: nil, albumName: "The Allusionist", trackName: "HSBC"),
         PlayerItem(itemURL: URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!, thumbnailURL: nil, albumName: nil, trackName: nil),
         PlayerItem(itemURL: URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")!, thumbnailURL: nil, albumName: "Elephant", trackName: "Dreams"),
-        PlayerItem(itemURL: URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!, thumbnailURL: nil, albumName: nil, trackName: nil),
+        PlayerItem(itemURL: URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4")!, thumbnailURL: nil, albumName: nil, trackName: nil),
         
     ]
 
